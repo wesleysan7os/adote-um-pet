f066e0e42cf2d0e3015bfdc8aadc76a731824847
Silva <wesley.santos.silva@ccc.ufcg.edu.br>
2022-06-01 22:27:53 -0300

    feat: Header component layout

--- src/ui/components/Header/Header.style.tsx
+++ src/ui/components/Header/Header.style.tsx
@@ -1,9 +1,12 @@
 import { styled } from '@mui/material';
 
 export const HeaderContainer = styled('header')`
-  
+  display: flex;
+  justify-content: center;
+  padding: ${({theme}) => theme.spacing(6)};
+  border-bottom: 1px solid #f0f0f0;
 `;
 
 export const Logo = styled('img')`
-
+  width: 230px;
 `;
