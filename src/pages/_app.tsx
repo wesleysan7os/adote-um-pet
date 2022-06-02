d165d3e514c16fce839b0ec0149b0c4591299616
Silva <wesley.santos.silva@ccc.ufcg.edu.br>
2022-06-01 22:20:24 -0300

    feat: create Header component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,15 @@
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from '@mui/material'
-import { theme } from '../ui/themes/theme';
+
+import { theme } from '../ui/themes/theme'
+import { Header } from '../ui/components/Header/Header'
 import '../ui/styles/globals.css'
 
-function MyApp({ Component, pageProps }: AppProps) {
+export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
+      <Header />
       <Component {...pageProps} />
     </ThemeProvider>
   );
 }
-
-export default MyApp
---
+++ src/ui/components/Header/Header.style.tsx
@@ -0,0 +1,9 @@
+import { styled } from '@mui/material';
+
+export const HeaderContainer = styled('header')`
+  
+`;
+
+export const Logo = styled('img')`
+
+`;
